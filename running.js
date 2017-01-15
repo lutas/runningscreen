4fd2a72cd7278690913e579e1c2035b17318ae4b
lutas <phil@lutas.co.uk>
2017-01-15 20:28:03 +0000

    Added first month data retrieval

--- running.js
+++ running.js
@@ -43,49 +43,56 @@ module.exports = {
         const from = year + '/' + month + '/01';
         const to = year + '/' + month + '/31';
 
-        var activitiesReceived = [];
-        var allDetails = [];
         var api = this.api;
-        api.fetchActivities(50, {'from': new Date(from), 'to': new Date(to)}, function(err, activities) {
-           
-            // flatten the activity ID's as they come back as array of arrays
-            var flattened = flatten(activities);
-            console.log("Month: " + month +  " - retrieved " + flattened.length + "/" + activities.length);
-
-            flattened.forEach(function(activityId) {
-
-                let activityReceivedPromise = new Promise(function(accept, reject) {
-
-                    api.fetchActivityDetails(activityId, false, function(err, activity) {
-
-                        if (err) {
-                            // expected that some might fail to retrieve but this is fine
-                            accept(false);
-                        }
-                        else {
-                            let attrib = activity.data.attributes;
-                            attrib.ext_data = undefined;
-                            attrib.fastest_paths = undefined;
-                            attrib.workout_data = undefined;
-                            attrib.zones = undefined;
-                            attrib.current_training_plan_state = undefined;
-
-                            details.push(attrib);
-                            accept(activity);
-                        }
-                    });
-                });
-
-                activitiesReceived.push(activityReceivedPromise);
-            });
-        });
 
         var completedPromise = new Promise(function(completedAccept, completedReject) {
-            Promise.all(activitesReceived).then(function() {
-                completedAccept(details);
-            }, function(err) {
-                completedReject(err);
-            })
+            api.fetchActivities(50, {'from': new Date(from), 'to': new Date(to)}, function(err, activities) {
+
+                if (err) {
+                    completedReject(err);
+                    return;
+                }
+               
+                // flatten the activity ID's as they come back as array of arrays
+                var flattened = flatten(activities);
+                console.log("Month: " + month +  " - retrieved " + flattened.length + "/" + activities.length);
+        
+                var activitiesReceived = [];
+                var allDetails = [];
+
+                flattened.forEach(function(activityId) {
+
+                    let activityReceivedPromise = new Promise(function(individualActivityAccept, individualActivityReject) {
+
+                        api.fetchActivityDetails(activityId, false, function(err, activity) {
+
+                            if (err) {
+                                // expected that some might fail to retrieve but this is fine
+                                individualActivityAccept(false);
+                            }
+                            else {
+                                let attrib = activity.data.attributes;
+                                attrib.ext_data = undefined;
+                                attrib.fastest_paths = undefined;
+                                attrib.workout_data = undefined;
+                                attrib.zones = undefined;
+                                attrib.current_training_plan_state = undefined;
+
+                                allDetails.push(attrib);
+                                individualActivityAccept(activity);
+                            }
+                        });
+                    });
+
+                    activitiesReceived.push(activityReceivedPromise);
+                });
+                
+                Promise.all(activitiesReceived).then(function() {
+                    completedAccept(allDetails);
+                }, function(err) {
+                    completedReject(err);
+                });
+            });            
         });
 
         return completedPromise;
