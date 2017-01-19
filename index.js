f2d0dcf6aebb40a2e250cb343c0ba3f17687d422
lutas <phil@lutas.co.uk>
2017-01-19 20:20:40 +0000

    Reduced code to ease debugging

--- index.js
+++ index.js
@@ -3,59 +3,68 @@
 
 console.log("app start");
 
-var config = require('./config.js');
-var runtasticManager = require('./running.js');
+//var config = require('./config.js');
 var display = require('./display.js');
 
-var YearStats = require("./yearstats.js");
+var config = {
+    emailAddress: process.env.LoginEmail,
+    password: process.env.Password
+};
 
-const prevYearValue = 2016; // deduce at some point
-const thisYearValue = 2017;
-
-var prevYear = new YearStats(prevYearValue);
-var thisYear = new YearStats(thisYearValue);
+console.log("Using account: " + config.emailAddress);
 
 setTimeout(function() {
     console.log("Clearing display");
-    display.clear();
-    display.init();
     console.log("Initialised display");
 
 }, 2000);
 
-console.log("Attempting login");
-runtasticManager.login(config).then(function() {
+setInterval(function() {
+    console.log("In interval");
+}, 60000);
 
-    var allMonths = [];
-    for (let monthIndex = 1; monthIndex <= 12; ++monthIndex) {
 
-        // previous year details - could cache these somewhere
-        let monthPromise = runtasticManager.getMonthStats(monthIndex, prevYearValue);        
-        monthPromise.then(function(data) {
-            prevYear.addStats(monthIndex, data);
-        }, display.error);
+    display.clear();
+    display.init();
 
-        allMonths.push(monthPromise);
 
-        // current year
-        let thisYearMonthPromise = runtasticManager.getMonthStats(monthIndex, thisYearValue);
-        thisYearMonthPromise.then(function(data) {
-            thisYear.addStats(monthIndex, data);
-        }, display.error);
+// var runtasticManager = require('./running.js');
 
-        allMonths.push(thisYearMonthPromise);
-    }
+// var YearStats = require("./yearstats.js");
 
-    Promise.all(allMonths).then(function() {
+// const prevYearValue = 2016; // deduce at some point
+// const thisYearValue = 2017;
 
-        display.output(1, prevYear, thisYear);
+// var prevYear = new YearStats(prevYearValue);
+// var thisYear = new YearStats(thisYearValue);
+// console.log("Attempting login");
+// runtasticManager.login(config).then(function() {
 
-    }, display.error);
+//     var allMonths = [];
+//     for (let monthIndex = 1; monthIndex <= 12; ++monthIndex) {
 
-}, display.error);
+//         // previous year details - could cache these somewhere
+//         let monthPromise = runtasticManager.getMonthStats(monthIndex, prevYearValue);        
+//         monthPromise.then(function(data) {
+//             prevYear.addStats(monthIndex, data);
+//         }, display.error);
 
-setTimeout(function() {
+//         allMonths.push(monthPromise);
 
-    console.log("Delaying application end");
+//         // current year
+//         let thisYearMonthPromise = runtasticManager.getMonthStats(monthIndex, thisYearValue);
+//         thisYearMonthPromise.then(function(data) {
+//             thisYear.addStats(monthIndex, data);
+//         }, display.error);
 
-}, 30000);
+//         allMonths.push(thisYearMonthPromise);
+//     }
+
+//     Promise.all(allMonths).then(function() {
+
+//         display.output(1, prevYear, thisYear);
+
+//     }, display.error);
+
+// }, display.error);
+
